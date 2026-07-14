90034d20709f7610b7272c7ef8c420d807dc254d
Dhruv Jain7 <djmjtj10@gmail.com>
2026-07-14 22:22:21 +0530

    Sarvam transcription done

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
 from core.transcriber import transcribe_all
 from utils.audio_processor import process_input
+import os
 
-source = "https://www.youtube.com/watch?v=ZVPlLaehjLk"
+print("KEY LOADED:" , os.getenv("SARVAM_API_KEY"))
+print("CWD", os.getcwd())
+source = "https://www.youtube.com/watch?v=OqyejuqLh14"
+language = "hinglish"
 
 chunks = process_input(source)
+transcript = transcribe_all(chunks,language=language)
 
-print(transcribe_all(chunks))
+print("\n === Transcript ===\n")
+print(transcript)
